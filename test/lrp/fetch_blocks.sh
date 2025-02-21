cdda40a92aa465056f0353dfeb0d74e9fde95f80
KyrinCode <kyrincode@qq.com>
2025-02-21 21:21:47 +0800

    re-organize

--- test/lrp/fetch_blocks.sh
+++ test/lrp/fetch_blocks.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# 检查是否提供了所有必要的参数
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
     echo "Usage: $0 --to <end_blocknumber> --rpc-url <url> [--from <start_blocknumber>] [-f <target_file>]"
     exit 1
 fi
 
-# 设置默认目标文件名
 START_BLOCKNUMBER=0
 TARGET_FILE="blocks_info.txt"
 
-# 读取参数
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --from) START_BLOCKNUMBER="$2"; shift ;;
@@ -22,21 +19,18 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# 检查是否所有参数都已设置
 if [ -z "$END_BLOCKNUMBER" ] || [ -z "$RPC_URL" ]; then
     echo "Usage: $0 --to <end_blocknumber> --rpc-url <url> [--from <start_blocknumber>] [-f <target_file>]"
     exit 1
 fi
 
-# 清空目标文件
 > $TARGET_FILE
 
-# 循环获取区块信息
 for blocknumber in $(seq $START_BLOCKNUMBER $END_BLOCKNUMBER)
 do
     echo "Fetching block $blocknumber..."
     cast block $blocknumber --rpc-url $RPC_URL >> $TARGET_FILE
-    echo "" >> $TARGET_FILE  # 添加一个空行以分隔每个区块的信息
+    echo "" >> $TARGET_FILE
 done
 
 echo "All blocks fetched and saved to $TARGET_FILE"
